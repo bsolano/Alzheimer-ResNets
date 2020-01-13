7fa3a77eca32bf9d8798f55e42b253035aa44fc3
Braulio Solano Rojas <braulio@solsoft.biz>
2020-01-13 12:47:04 -0600

    Statistics

--- alzheimer_model.py
+++ alzheimer_model.py
@@ -99,8 +99,8 @@ def test():
 
             # print statistics
             running_loss += loss.item()
-            if i % 200 == 199:    # print every 200 batches
-                print('[%d, %5d] pérdida: %.3f' % (epoch + 1, i + 1, running_loss / 200))
+            if i % 10 == 9:    # print every 10 batches
+                print('[epoch %d, batch %5d] pérdida: %.3f' % (epoch + 1, i + 1, running_loss / 200))
                 running_loss = 0.0
 
     model.eval()
