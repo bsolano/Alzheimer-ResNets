09a7e0bb8e5aa7bceba8372937750edd28065c0f
Braulio Solano Rojas <braulio@solsoft.biz>
2020-01-12 21:39:52 -0600

    Performance improvements

--- transforms.py
+++ transforms.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy
 import skimage.transform
 import torch
+from numba import jit
 
 class ToTensor(object):
     """Convert pydicom image in sample to Tensors."""
@@ -17,6 +18,7 @@ class ToTensor(object):
         self.aspect = aspect
         self.cut = cut
 
+    #@jit(nopython=True, parallel=True)
     def __call__(self, sample):
         # Las imágenes son de un único color en tonos.  Al color se le suele llamar canal.
         # Así, deberíamos devolver un tensor de este tipo:  C X S X H X W,
@@ -34,20 +36,22 @@ class ToTensor(object):
         if self.spacing is not None:
             # Nuevo tamaño de los voxel
             new_spacing = self.spacing
-    
+
             # Tamaño actual de los voxel
             spacing = map(float, ([sample[0].SliceThickness] + list(sample[0].PixelSpacing)))
             spacing = np.array(list(spacing))
-        
-            # Cálculo de las nuevas proporciones
-            resize_factor = spacing / new_spacing
-            new_real_shape = image.shape * resize_factor
-            new_shape = np.round(new_real_shape)
-            real_resize_factor = new_shape / image.shape
-            new_spacing = spacing / real_resize_factor
-            
-            # Reconstrucción de la imagen al nuevo tamaño de voxel
-            image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+
+            # Si los espaciados son diferentes
+            if spacing[0] != new_spacing[0] and spacing[1] != new_spacing[1] and spacing[2] != new_spacing[2]:
+                # Cálculo de las nuevas proporciones
+                resize_factor = spacing / new_spacing
+                new_real_shape = image.shape * resize_factor
+                new_shape = np.round(new_real_shape)
+                real_resize_factor = new_shape / image.shape
+                new_spacing = spacing / real_resize_factor
+
+                # Reconstrucción de la imagen al nuevo tamaño de voxel
+                image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, order=1)
 
         # Siempre convertimos cada corte a 256x256, si el corte no es cuadrado
         # lo rellenamos.
@@ -64,45 +68,29 @@ class ToTensor(object):
                 image = np.insert(image, width, [0], axis=1)
             for i in range(w):
                 image = np.insert(image, 0, [0], axis=1)
-        real_resize_factor = np.array([deep, 256, 256]) / image.shape
         # Reconstrucción de la imagen al nuevo tamaño de voxel
         if image.shape[1] != 256 and image.shape[2] != 256:
-            image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+            real_resize_factor = np.array([deep, 256, 256]) / image.shape
+            image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, order=1)
 
         # Establecemos el número de cortes:
-        if self.num_slices is not None:
-            #if 'spacing' in locals() and 'new_spacing' in locals():
-            #    spacing = new_spacing
-            #else:
-            #    # Tamaño actual de los voxel
-            #    spacing = map(float, ([sample[0].SliceThickness] + sample[0].PixelSpacing))
-            #    spacing = np.array(list(spacing))
-                
-            #new_spacing = spacing
-            #new_spacing[0] = self.num_slices/image.shape[0]
-
-            # Cálculo de las nuevas proporciones
-            #resize_factor = spacing / new_spacing
-            #new_real_shape = image.shape * resize_factor
-            #new_shape = np.round(new_real_shape)
-            #real_resize_factor = new_shape / image.shape
-            #new_spacing = spacing / real_resize_factor
-            
+        if self.num_slices is not None:            
             (deep, height, width) = image.shape
-            real_resize_factor = np.array([self.num_slices, height, width]) / image.shape
 
-            # Reconstrucción de la imagen al nuevo tamaño de voxel
-            image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+            if self.num_slices != deep:
+                real_resize_factor = np.array([self.num_slices, height, width]) / image.shape
+
+                # Reconstrucción de la imagen al nuevo tamaño de voxel
+                image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, order=1)
 
         # Un canal:
         if self.aspect == 'sagittal':
-            pass
+            channel = [image[self.cut[0], self.cut[1], self.cut[2]]]
         elif self.aspect == 'axial':
             image = np.rot90(image)
             image = np.rot90(image, k=-1, axes=(1,2))
             channel = [image[self.cut[0], self.cut[1], self.cut[2]]]
         elif self.aspect == 'coronal':
             pass
-
 
         return torch.from_numpy(np.array(channel))
