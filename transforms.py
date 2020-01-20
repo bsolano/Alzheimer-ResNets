256db3d1006a330b1db15bda18ce58feb2edb4ea
Braulio Solano Rojas <braulio@solsoft.biz>
2020-01-20 00:44:03 -0600

    Test a variable in the exception

--- transforms.py
+++ transforms.py
@@ -53,7 +53,7 @@ class ToTensor(object):
             except Exception as e:
                 global la_imagen
                 la_imagen = sample[0]
-                print(sample[0][0x52009230])
+                print(sample[0][0x52009230][0x00289110][0x00180050].value)
                 raise e
 
             # Si los espaciados son diferentes
