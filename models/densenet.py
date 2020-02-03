fe5ee11ca13657fd00736881433b2f8878a00191
Braulio Solano Rojas <braulio@solsoft.biz>
2020-02-02 22:09:25 -0600

    Not needed

--- models/densenet.py
+++ models/densenet.py
@@ -196,7 +196,7 @@ class DenseNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv3d):
                 m.weight = nn.init.kaiming_normal_(m.weight, mode='fan_out')
-            elif isinstance(m, nn.BatchNorm3d) or isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, nn.BatchNorm3d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
