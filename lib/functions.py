925d0386a641233b0a052ce87ab12afa215f1c29
Braulio Solano Rojas <braulio@solsoft.biz>
2020-02-05 16:54:12 -0600

    Missing imports

--- lib/functions.py
+++ lib/functions.py
@@ -1,4 +1,7 @@
 from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import auc
 from sklearn.metrics import roc_curve
@@ -37,6 +40,7 @@ def plot_loss(losses):
     plt.ylabel('loss')
     plt.show()
     plt.close()
+
 
 def plot_ROC_curve(true, predicted, classes):
     """
