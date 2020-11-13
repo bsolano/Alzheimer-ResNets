6f2a63e13a5a852c9d0b252f44b26dc37c0b124c
Braulio J. Solano-Rojas <braulio@solsoft.biz>
2020-11-13 12:26:00 -0600

    Change optimizer:  Rprop to RMSprop

--- alzheimer_model.py
+++ alzheimer_model.py
@@ -79,7 +79,7 @@ def test(class_names, data_dir, results_dir, epochs, batch_size, lr_decay_epochs
 
     # Optimizador:
     # Estas son optimizaciones al algoritmo de descenso por gradiente para evitar mínimos locales en la búsqueda.
-    optimizer = torch.optim.Rprop(model.parameters()) # Resilient backpropagation algorithm
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=0.1, momentum=0.9) # Root Mean Square backpropagation algorithm
 
     # Ciclo de entrenamiento:
     losses = []
